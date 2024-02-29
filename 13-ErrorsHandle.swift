21efa5c3ffde7b099a07483a2e17fc3500812d73
AsadAnik <s.engineer63@gmail.com>
2024-02-29 21:22:21 +0600

    Swift Functions and Throw Errors

--- 13-ErrorsHandle.swift
+++ 13-ErrorsHandle.swift
@@ -1,1 +1,52 @@
 // LETS SEE HOW TO HANDLE ERRORS ON SWIFT..
+enum PasswordError: Error {
+    case short, obvious
+}
+
+func checkPassword(_ password: String) throws -> String {
+    if password.count < 5 {
+        throw PasswordError.short
+    }
+
+    if password == "12345" {
+        throw PasswordError.obvious
+    }
+
+    if password.count > 8 {
+        return "OK";
+
+    } else if password.count > 10 {
+        return "Good";
+
+    } else {
+        return "Excellent";
+    }
+}
+
+let myPass = "12345";
+
+// Try the thing if there any errors to receive..
+do {
+    let result = try checkPassword(myPass);
+    print("Password rating: \(result)");
+} catch {
+    print("There was an error: \(error)");
+}
+
+
+// ANOTHER WAY OF HANDLING ERRORS..
+let myAnotherPass = "1234";
+
+do {
+    let result = try checkPassword(myAnotherPass);
+    print("Password rating: \(result)");
+
+} catch PasswordError.short {
+    print("Short password");
+
+} catch PasswordError.obvious {
+    print("Obvious password");
+
+} catch {
+    print("There was an error");
+}
